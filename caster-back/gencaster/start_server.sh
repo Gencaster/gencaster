0bb593d5b1f2b015ec1c66747b9f3145dc5d1635
Dennis Scheiba <dennis.scheiba@gmx.de>
2022-02-17 09:08:58 +0100

    init django backend

---
+++ caster-back/gencaster/start_server.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+sleep 2;
+
+echo "Apply database migrations";
+python manage.py migrate;
+
+echo "Check if SuperUser exists";
+if [ -z "$DJANGO_SU_USER" ];
+then
+    echo "No Superuser given";
+else
+    python_script="from django.contrib.auth import get_user_model; User = get_user_model();  User.objects.create_superuser('"$DJANGO_SU_USER"', '', '"$DJANGO_SU_PASS"') if not User.objects.filter(username='"$DJANGO_SU_USER"').exists() else print('Superuser exists already')";
+    python manage.py shell --command "$python_script";
+fi;
+
+echo "Start development server";
+python manage.py runserver 0.0.0.0:8080;
