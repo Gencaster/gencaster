b8cc22215c00ec73ec6f08d02ab9ea7b3b7ad7b3
Dennis Scheiba <dennis.scheiba@gmx.de>
2022-05-27 22:26:12 +0200

    Reset all active Streams on start

--- caster-back/stream/apps.py
+++ caster-back/stream/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class StreamConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "stream"
+
+    def ready(self) -> None:
+        from .models import Stream
+
+        Stream.objects.disconnect_all_streams()
+        return super().ready()
