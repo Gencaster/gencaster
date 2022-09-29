9a7403a5b0d534b9dc93fda747efbb2a225cf880
Vinzenz Aubry <vinzenzaubry@gmail.com>
2022-09-30 00:04:37 +0200

    Added the clicking demo into the index

--- caster-editor/pages/index.vue
+++ caster-editor/pages/index.vue
@@ -1,23 +1,78 @@
 <script setup lang="ts">
-const nodes = {
-  node1: { name: 'Node 1' },
-  node2: { name: 'Node 2' },
-  node3: { name: 'Node 3' },
-  node4: { name: 'Node 4' },
+import { reactive, ref } from 'vue';
+import { Nodes, Edges } from 'v-network-graph';
+import data from '~/assets/data/graphData';
+
+const nodes: Nodes = reactive({ ...data.nodes });
+const edges: Edges = reactive({ ...data.edges });
+const nextNodeIndex = ref(Object.keys(nodes).length + 1);
+const nextEdgeIndex = ref(Object.keys(edges).length + 1);
+
+const selectedNodes = ref<string[]>([]);
+const selectedEdges = ref<string[]>([]);
+
+const addNode = () => {
+  const nodeId = `node${nextNodeIndex.value}`;
+  const name = `N${nextNodeIndex.value}`;
+  nodes[nodeId] = { name };
+  nextNodeIndex.value++;
 };
 
-const edges = {
-  edge1: { source: 'node1', target: 'node2' },
-  edge2: { source: 'node2', target: 'node3' },
-  edge3: { source: 'node3', target: 'node4' },
-};
+function removeNode() {
+  for (const nodeId of selectedNodes.value) {
+    delete nodes[nodeId];
+  }
+}
+
+function addEdge() {
+  if (selectedNodes.value.length !== 2) return;
+  const [source, target] = selectedNodes.value;
+  const edgeId = `edge${nextEdgeIndex.value}`;
+  edges[edgeId] = { source, target };
+  nextEdgeIndex.value++;
+}
+
+function removeEdge() {
+  for (const edgeId of selectedEdges.value) {
+    delete edges[edgeId];
+  }
+}
 </script>
 
 <template>
   <div class="index-page">
     <div>
       <h1>Welcome to the Editor</h1>
+      <br />
     </div>
-    <v-network-graph class="graph" :nodes="nodes" :edges="edges" />
+    <div class="demo-control-panel">
+      <div>
+        <label>Node:</label>
+        <button @click="addNode">add</button>
+        <button :disabled="selectedNodes.length == 0" @click="removeNode">
+          remove
+        </button>
+      </div>
+      <div>
+        <label>Edge:</label>
+        <button :disabled="selectedNodes.length != 2" @click="addEdge">
+          add
+        </button>
+        <button :disabled="selectedEdges.length == 0" @click="removeEdge">
+          remove
+        </button>
+      </div>
+    </div>
+    <br />
+
+    <v-network-graph
+      class="graph"
+      v-model:selected-nodes="selectedNodes"
+      v-model:selected-edges="selectedEdges"
+      :nodes="nodes"
+      :edges="edges"
+      :layouts="data.layouts"
+      :configs="data.configs"
+    />
   </div>
 </template>
