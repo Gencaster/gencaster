a77ee17b3b509c79cb04833046a9606fb2954b15
Vinzenz Aubry <vinzenzaubry@gmail.com>
2022-10-31 16:05:47 +0100

    working fix

--- caster-editor/components/Graph.vue
+++ caster-editor/components/Graph.vue
@@ -1,9 +1,8 @@
-<!-- <script lang="ts">
+<script lang="ts">
 import { defineComponent, toRefs, ref, reactive } from 'vue';
 import { useGetGraphQuery, useCreateNodeMutation } from '../graphql/graphql';
 import * as vNG from 'v-network-graph';
 import { transformEdges, transformNodes } from '../tools/typeTransformers';
-import * as Urql from '@urql/vue';
 
 export default defineComponent({
   props: {
@@ -11,100 +10,67 @@ export default defineComponent({
   },
 
   setup(props) {
-    // access uuid
     const uuid = toRefs(props).uuid;
 
-    // editor config
+    const selectedNodes = ref<string[]>([]);
+    const selectedEdges = ref<string[]>([]);
     const configs = reactive(vNG.getFullConfigs());
     configs.node.selectable = true;
 
-    // setup node & edge data
-    const selectedNodes = ref<string[]>([]);
-    const selectedEdges = ref<string[]>([]);
+    const { executeMutation: newMutation } = useCreateNodeMutation();
+    // const foo = useCreateNodeMutation();
 
-    // const executeMutation = useCreateNodeMutation;
-    const { executeMutation: updateGraph } = useCreateNodeMutation;
-    console.log(useCreateNodeMutation);
+    function addNode(event) {
+      const singleid = Math.round(Math.random() * 1000000);
+      console.log('start adding note', event);
+      const variables = {
+        graphUuid: uuid,
+        name: `Some new random name ${singleid}`,
+      };
+      newMutation(variables).then(() => {
+        console.log('Hello, we are finished');
+      });
+    }
 
-    // const { executeMutation: updateTodo } = Urql.useMutation(`
-    //   mutation ($id: ID!, $title: String!) {
-    //     updateTodo (id: $id, title: $title) {
-    //       id
-    //       title
-    //     }
-    //   }
-    // `);
+    // function addNode(event){
+    //   console.log(event);
+    //   const variables = {graphUuid: uuid, name: "Some new random name"};
+    //     useCreateNodeMutation()
+    //       .executeMutation(variables)
+    //       .then(() => {
+    //         console.log("Please refresh");
+    //         refresh();
+    //       });
+    // }
 
-    // load data
     const result = useGetGraphQuery({
       variables: { uuid: uuid },
       requestPolicy: 'network-only',
     });
 
-    // data functions
     const refresh = () => {
       console.log('Rerfresh');
       result.executeQuery();
     };
 
-    // interface  functions
-    const addNode = async () => {
-      try {
-        const variables = { graphUuid: uuid, name: 'Some new random name' };
-        // console.log(result);
-
-        // // console.log(result.isPaused);
-        // // result.pause();
-        // console.log(result.isPaused);
-
-        // console.log(executeMutation);
-
-        // useCreateNodeMutation();
-
-        // executeMutation()
-
-        console.log(updateGraph);
-
-        useCreateNodeMutation()
-          .executeMutation(variables)
-          .then(() => {
-            // console.log('Please refresh');
-            // refresh();
-          });
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    const removeNode = async () => {
-      console.log('removeNode');
-    };
-
-    // let isPaused = ref(false);
-
-    // export for scope
     return {
-      // isPaused,
       fetching: result.fetching,
       data: result.data,
       error: result.error,
-      refresh() {
-        result.executeQuery({
-          requestPolicy: 'network-only',
-        });
-      },
       transformEdges,
       transformNodes,
       selectedNodes,
       selectedEdges,
       configs,
-      updateGraph,
+      refresh,
       addNode,
-      removeNode,
+      removeNode() {
+        console.log('removeNode');
+      },
     };
   },
 });
-</script> -->
+</script>
 
 <template>
   <div class="index-page">
@@ -117,11 +83,10 @@ export default defineComponent({
         <div>
           <label>Node:</label>
           {{ data.graph.nodes.length }}
-          <!-- <button @click="isPaused ? resume() : pause()">Toggle Query</button> -->
-          <button :disabled="selectedNodes.length == 0" @click="removeNode">
-            Remove
+          <button :disabled="selectedNodes.length == 0" @click="removeNode()">
+            remove
           </button>
-          <button @click="addNode">Add</button>
+          <button @click="addNode">Create</button>
         </div>
         <div>
           <label>Edge:</label>
@@ -136,91 +101,6 @@ export default defineComponent({
         :edges="transformEdges(data.graph.edges)"
         :configs="configs"
       />
-
-      <p>{{ data.graph }}</p>
     </div>
   </div>
 </template>
-
-<script lang="ts">
-import { useGetGraphQuery, useCreateNodeMutation } from '../graphql/graphql';
-import * as vNG from 'v-network-graph';
-import { transformEdges, transformNodes } from '../tools/typeTransformers';
-
-export default {
-  name: 'graphComponent',
-
-  props: {
-    uuid: {
-      type: String,
-      required: true,
-    },
-  },
-
-  data() {
-    return {
-      fetching: true,
-      data: null,
-      error: null,
-
-      transformEdges,
-      transformNodes,
-
-      selectedNodes: [],
-      selectedEdges: [],
-      configs: vNG.getFullConfigs(),
-    };
-  },
-  mounted() {
-    console.log(this.uuid);
-
-    this.configs.node.selectable = true;
-    // const { executeMutation: newMutation } = useCreateNodeMutation;
-
-    // get async data
-    this.loadData();
-
-    // setTimeout(() => {
-    //   console.log(this.selectedNodes);
-    // }, 2000);
-  },
-  methods: {
-    async loadData() {
-      const result = await useGetGraphQuery({
-        variables: { uuid: this.uuid },
-        requestPolicy: 'network-only',
-      });
-
-      this.data = result.data;
-      this.error = result.error;
-      this.fetching = result.fetching;
-      console.log(this.selectedNodes);
-    },
-
-    refresh() {
-      console.log('Rerfresh');
-      this.result.executeQuery();
-    },
-
-    async removeNode() {
-      console.log('removeNode');
-    },
-
-    async addNode() {
-      console.log('addNode');
-      const variables = { graphUuid: this.uuid, name: 'Some new random name' };
-
-      try {
-        useCreateNodeMutation()
-          .executeMutation(variables)
-          .then(() => {
-            // console.log('Please refresh');
-            // this.refresh();
-          });
-      } catch (err) {
-        console.log(err);
-      }
-    },
-  },
-};
-</script>
