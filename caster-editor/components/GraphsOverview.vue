5f336045138f3ebd7aafa92c8bf1a83702fdca32
Vinzenz Aubry <vinzenzaubry@gmail.com>
2022-11-04 17:04:32 +0100

    wrote loading component

--- caster-editor/components/GraphsOverview.vue
+++ caster-editor/components/GraphsOverview.vue
@@ -1,14 +1,14 @@
-<script lang="ts" setup>
+<!-- <script lang="ts" setup>
 import { Edit } from '@element-plus/icons-vue';
-</script>
+</script> -->
+<!-- <el-switch v-model="value1" /> -->
+<!-- <el-config-provider size="small">
+      <el-button type="primary" :icon="Edit" />
+    </el-config-provider> -->
 
 <template>
   <div class="index-page">
-    <!-- <el-switch v-model="value1" /> -->
-    <!-- <el-config-provider size="small">
-      <el-button type="primary" :icon="Edit" />
-    </el-config-provider> -->
-    <div v-if="fetching">...Loading</div>
+    <div class="fetching-screen" v-if="fetching"><elementsLoading /></div>
     <div v-else>
       <h1>Please select a graph</h1>
       <div class="demo-control-panel">
---
+++ caster-editor/components/elements/Loading.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="fetching-screen">
+    <p>Loading...</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.fetching-screen {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
