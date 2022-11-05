80c4c1c4ad643d297b47ba18f23331027185f43b
Dennis Scheiba <dennis.scheiba@gmx.de>
2022-11-05 16:50:13 +0100

    add delete mutations in schemes

--- caster-editor/graphql/graphql.ts
+++ caster-editor/graphql/graphql.ts
@@ -2,15 +2,9 @@ import gql from 'graphql-tag';
 import * as Urql from '@urql/vue';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = {
-  [K in keyof T]: T[K];
-};
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]?: Maybe<T[SubKey]>;
-};
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
-  [SubKey in K]: Maybe<T[SubKey]>;
-};
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
@@ -68,14 +62,28 @@ export type Mutation = {
   __typename?: 'Mutation';
   addEdge?: Maybe<Scalars['Void']>;
   addNode?: Maybe<Scalars['Void']>;
+  deleteEdge?: Maybe<Scalars['Void']>;
+  deleteNode?: Maybe<Scalars['Void']>;
 };
+
 
 export type MutationAddEdgeArgs = {
   newEdge: EdgeInput;
 };
 
+
 export type MutationAddNodeArgs = {
   newNode: NodeInput;
+};
+
+
+export type MutationDeleteEdgeArgs = {
+  edgeUuid: Scalars['UUID'];
+};
+
+
+export type MutationDeleteNodeArgs = {
+  nodeUuid: Scalars['UUID'];
 };
 
 export type Node = {
@@ -102,17 +110,21 @@ export type Query = {
   streamPoints: Array<StreamPoint>;
 };
 
+
 export type QueryGraphArgs = {
   pk?: InputMaybe<Scalars['ID']>;
 };
+
 
 export type QueryNodeArgs = {
   pk?: InputMaybe<Scalars['ID']>;
 };
 
+
 export type QueryStreamPointArgs = {
   pk?: InputMaybe<Scalars['ID']>;
 };
+
 
 export type QueryStreamPointsArgs = {
   filters?: InputMaybe<StreamPointFilter>;
@@ -150,6 +162,7 @@ export type Subscription = {
   count: Scalars['Int'];
 };
 
+
 export type SubscriptionCountArgs = {
   target?: Scalars['Int'];
 };
@@ -174,188 +187,163 @@ export type UuidFilterLookup = {
   startsWith?: InputMaybe<Scalars['UUID']>;
 };
 
-export type MyQueryQueryVariables = Exact<{ [key: string]: never }>;
+export type MyQueryQueryVariables = Exact<{ [key: string]: never; }>;
 
-export type MyQueryQuery = {
-  __typename?: 'Query';
-  graphs: Array<{ __typename?: 'Graph'; name: string }>;
-};
 
-export type TestQueryQueryVariables = Exact<{ [key: string]: never }>;
+export type MyQueryQuery = { __typename?: 'Query', graphs: Array<{ __typename?: 'Graph', name: string }> };
 
-export type TestQueryQuery = {
-  __typename?: 'Query';
-  graphs: Array<{
-    __typename?: 'Graph';
-    name: string;
-    nodes: Array<{ __typename?: 'Node'; name: string }>;
-    edges: Array<{ __typename?: 'Edge'; uuid: any }>;
-  }>;
-};
+export type TestQueryQueryVariables = Exact<{ [key: string]: never; }>;
 
-export type GetGraphsQueryVariables = Exact<{ [key: string]: never }>;
 
-export type GetGraphsQuery = {
-  __typename?: 'Query';
-  graphs: Array<{ __typename?: 'Graph'; uuid: any; name: string }>;
-};
+export type TestQueryQuery = { __typename?: 'Query', graphs: Array<{ __typename?: 'Graph', name: string, nodes: Array<{ __typename?: 'Node', name: string }>, edges: Array<{ __typename?: 'Edge', uuid: any }> }> };
+
+export type GetGraphsQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type GetGraphsQuery = { __typename?: 'Query', graphs: Array<{ __typename?: 'Graph', uuid: any, name: string }> };
 
 export type GetGraphQueryVariables = Exact<{
   uuid?: InputMaybe<Scalars['ID']>;
 }>;
 
-export type GetGraphQuery = {
-  __typename?: 'Query';
-  graph: {
-    __typename?: 'Graph';
-    name: string;
-    uuid: any;
-    edges: Array<{
-      __typename?: 'Edge';
-      uuid: any;
-      outNode: { __typename?: 'Node'; uuid: any };
-      inNode: { __typename?: 'Node'; uuid: any };
-    }>;
-    nodes: Array<{
-      __typename?: 'Node';
-      name: string;
-      uuid: any;
-      scriptCells: Array<{
-        __typename?: 'ScriptCell';
-        cellCode: string;
-        cellOrder: number;
-        cellType: string;
-        uuid: any;
-      }>;
-    }>;
-  };
-};
+
+export type GetGraphQuery = { __typename?: 'Query', graph: { __typename?: 'Graph', name: string, uuid: any, edges: Array<{ __typename?: 'Edge', uuid: any, outNode: { __typename?: 'Node', uuid: any }, inNode: { __typename?: 'Node', uuid: any } }>, nodes: Array<{ __typename?: 'Node', name: string, uuid: any, scriptCells: Array<{ __typename?: 'ScriptCell', cellCode: string, cellOrder: number, cellType: string, uuid: any }> }> } };
 
 export type CreateEdgeMutationVariables = Exact<{
   nodeInUuid: Scalars['UUID'];
   nodeOutUuid: Scalars['UUID'];
 }>;
 
-export type CreateEdgeMutation = {
-  __typename?: 'Mutation';
-  addEdge?: any | null;
-};
+
+export type CreateEdgeMutation = { __typename?: 'Mutation', addEdge?: any | null };
 
 export type CreateNodeMutationVariables = Exact<{
   name: Scalars['String'];
   graphUuid: Scalars['UUID'];
 }>;
 
-export type CreateNodeMutation = {
-  __typename?: 'Mutation';
-  addNode?: any | null;
-};
+
+export type CreateNodeMutation = { __typename?: 'Mutation', addNode?: any | null };
+
+export type DeleteNodeMutationVariables = Exact<{
+  nodeUuid: Scalars['UUID'];
+}>;
+
+
+export type DeleteNodeMutation = { __typename?: 'Mutation', deleteNode?: any | null };
+
+export type DeleteEdgeMutationVariables = Exact<{
+  edgeUuid: Scalars['UUID'];
+}>;
+
+
+export type DeleteEdgeMutation = { __typename?: 'Mutation', deleteEdge?: any | null };
+
 
 export const MyQueryDocument = gql`
-  query MyQuery {
-    graphs {
-      name
-    }
+    query MyQuery {
+  graphs {
+    name
   }
-`;
+}
+    `;
 
-export function useMyQueryQuery(
-  options: Omit<Urql.UseQueryArgs<never, MyQueryQueryVariables>, 'query'> = {}
-) {
+export function useMyQueryQuery(options: Omit<Urql.UseQueryArgs<never, MyQueryQueryVariables>, 'query'> = {}) {
   return Urql.useQuery<MyQueryQuery>({ query: MyQueryDocument, ...options });
-}
+};
 export const TestQueryDocument = gql`
-  query testQuery {
-    graphs {
-      nodes {
-        name
-      }
-      edges {
-        uuid
-      }
+    query testQuery {
+  graphs {
+    nodes {
       name
     }
+    edges {
+      uuid
+    }
+    name
   }
-`;
-
-export function useTestQueryQuery(
-  options: Omit<Urql.UseQueryArgs<never, TestQueryQueryVariables>, 'query'> = {}
-) {
-  return Urql.useQuery<TestQueryQuery>({
-    query: TestQueryDocument,
-    ...options,
-  });
 }
+    `;
+
+export function useTestQueryQuery(options: Omit<Urql.UseQueryArgs<never, TestQueryQueryVariables>, 'query'> = {}) {
+  return Urql.useQuery<TestQueryQuery>({ query: TestQueryDocument, ...options });
+};
 export const GetGraphsDocument = gql`
-  query GetGraphs {
-    graphs {
-      uuid
-      name
-    }
+    query GetGraphs {
+  graphs {
+    uuid
+    name
   }
-`;
-
-export function useGetGraphsQuery(
-  options: Omit<Urql.UseQueryArgs<never, GetGraphsQueryVariables>, 'query'> = {}
-) {
-  return Urql.useQuery<GetGraphsQuery>({
-    query: GetGraphsDocument,
-    ...options,
-  });
 }
+    `;
+
+export function useGetGraphsQuery(options: Omit<Urql.UseQueryArgs<never, GetGraphsQueryVariables>, 'query'> = {}) {
+  return Urql.useQuery<GetGraphsQuery>({ query: GetGraphsDocument, ...options });
+};
 export const GetGraphDocument = gql`
-  query getGraph($uuid: ID) {
-    graph(pk: $uuid) {
+    query getGraph($uuid: ID) {
+  graph(pk: $uuid) {
+    name
+    uuid
+    edges {
+      uuid
+      outNode {
+        uuid
+      }
+      inNode {
+        uuid
+      }
+    }
+    nodes {
       name
       uuid
-      edges {
+      scriptCells {
+        cellCode
+        cellOrder
+        cellType
         uuid
-        outNode {
-          uuid
-        }
-        inNode {
-          uuid
-        }
-      }
-      nodes {
-        name
-        uuid
-        scriptCells {
-          cellCode
-          cellOrder
-          cellType
-          uuid
-        }
       }
     }
   }
-`;
-
-export function useGetGraphQuery(
-  options: Omit<Urql.UseQueryArgs<never, GetGraphQueryVariables>, 'query'> = {}
-) {
-  return Urql.useQuery<GetGraphQuery>({ query: GetGraphDocument, ...options });
 }
+    `;
+
+export function useGetGraphQuery(options: Omit<Urql.UseQueryArgs<never, GetGraphQueryVariables>, 'query'> = {}) {
+  return Urql.useQuery<GetGraphQuery>({ query: GetGraphDocument, ...options });
+};
 export const CreateEdgeDocument = gql`
-  mutation createEdge($nodeInUuid: UUID!, $nodeOutUuid: UUID!) {
-    addEdge(newEdge: { nodeInUuid: $nodeInUuid, nodeOutUuid: $nodeOutUuid })
-  }
-`;
+    mutation createEdge($nodeInUuid: UUID!, $nodeOutUuid: UUID!) {
+  addEdge(newEdge: {nodeInUuid: $nodeInUuid, nodeOutUuid: $nodeOutUuid})
+}
+    `;
 
 export function useCreateEdgeMutation() {
-  return Urql.useMutation<CreateEdgeMutation, CreateEdgeMutationVariables>(
-    CreateEdgeDocument
-  );
-}
-
+  return Urql.useMutation<CreateEdgeMutation, CreateEdgeMutationVariables>(CreateEdgeDocument);
+};
 export const CreateNodeDocument = gql`
-  mutation createNode($name: String!, $graphUuid: UUID!) {
-    addNode(newNode: { name: $name, graphUuid: $graphUuid })
-  }
-`;
+    mutation createNode($name: String!, $graphUuid: UUID!) {
+  addNode(newNode: {name: $name, graphUuid: $graphUuid})
+}
+    `;
 
 export function useCreateNodeMutation() {
-  return Urql.useMutation<CreateNodeMutation, CreateNodeMutationVariables>(
-    CreateNodeDocument
-  );
+  return Urql.useMutation<CreateNodeMutation, CreateNodeMutationVariables>(CreateNodeDocument);
+};
+export const DeleteNodeDocument = gql`
+    mutation deleteNode($nodeUuid: UUID!) {
+  deleteNode(nodeUuid: $nodeUuid)
 }
+    `;
+
+export function useDeleteNodeMutation() {
+  return Urql.useMutation<DeleteNodeMutation, DeleteNodeMutationVariables>(DeleteNodeDocument);
+};
+export const DeleteEdgeDocument = gql`
+    mutation deleteEdge($edgeUuid: UUID!) {
+  deleteEdge(edgeUuid: $edgeUuid)
+}
+    `;
+
+export function useDeleteEdgeMutation() {
+  return Urql.useMutation<DeleteEdgeMutation, DeleteEdgeMutationVariables>(DeleteEdgeDocument);
+};
