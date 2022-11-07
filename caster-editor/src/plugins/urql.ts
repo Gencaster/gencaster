69bd7bd9bbfd4793b90c340d9b2bc43d04121c68
Dennis Scheiba <dennis.scheiba@gmx.de>
2022-11-08 00:33:06 +0100

    use cookies for graphql

--- caster-editor/src/plugins/urql.ts
+++ caster-editor/src/plugins/urql.ts
@@ -7,5 +7,8 @@ export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(urql, {
         url: config.public.BACKEND_GRAPHQL_URL,
         requestPolicy: 'network-only',
+        fetchOptions: {
+            credentials: 'include'
+          }
     });
 });
