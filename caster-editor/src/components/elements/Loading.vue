8aea6e16963a78fe1cff5be8c480cab0ea7cf792
Vinzenz Aubry <vinzenzaubry@gmail.com>
2022-11-18 12:36:23 +0100

    rewrote loading screen

--- caster-editor/src/components/elements/Loading.vue
+++ caster-editor/src/components/elements/Loading.vue
@@ -3,17 +3,3 @@
     <p>Loading...</p>
   </div>
 </template>
-
-<style lang="scss" scoped>
-.fetching-screen {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  background-color: white;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-</style>
