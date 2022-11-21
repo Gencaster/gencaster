bfbbdd8a80d4d4970d408484f52256d90dab49ee
Dennis Scheiba <dennis.scheiba@gmx.de>
2022-11-21 01:05:14 +0100

    execute markdown cells in graph engine

--- caster-back/story_graph/engine.py
+++ caster-back/story_graph/engine.py
@@ -1,17 +1,28 @@
 import asyncio
+import logging
 
 from asgiref.sync import sync_to_async
+from mistletoe import Document
 
 from stream.models import StreamInstruction, StreamPoint
 
+from .markdown_parser import GencasterRenderer
 from .models import Graph, Node, ScriptCell
+
+log = logging.getLogger(__name__)
 
 
 class Engine:
     def __init__(self, graph: Graph, streaming_point: StreamPoint) -> None:
         self.graph: Graph = graph
-        self.streaming_point = streaming_point
+        self.streaming_point: StreamPoint = streaming_point
         self._current_node: Node
+
+    def execute_markdown_code(self, cell_code: str):
+        with GencasterRenderer() as render:
+            document = Document(cell_code)
+            ssml_text = render.render(document)
+        self.streaming_point.speak_on_stream(ssml_text)
 
     async def execute_sc_code(self, cell_code: str):
         instruction = await sync_to_async(self.streaming_point.send_raw_instruction)(
@@ -30,13 +41,17 @@ class Engine:
         async for script_cell in node.script_cells.all():  # type: ignore
             cell_type = script_cell.cell_type
             if cell_type == ScriptCell.CellType.COMMENT:
-                pass
-            if cell_type == ScriptCell.CellType.PYTHON:
+                continue
+            elif cell_type == ScriptCell.CellType.PYTHON:
                 print("Python should now execute: ", script_cell.cell_code)
                 if script_cell.cell_code:
                     exec(script_cell.cell_code)
-            if cell_type == ScriptCell.CellType.SUPERCOLLIDER:
+            elif cell_type == ScriptCell.CellType.SUPERCOLLIDER:
                 await self.execute_sc_code(script_cell.cell_code)
+            elif cell_type == ScriptCell.CellType.MARKDOWN:
+                await sync_to_async(self.execute_markdown_code)(script_cell.cell_code)
+            else:
+                log.error(f"Occured invalid/unknown CellType {cell_type}")
 
     async def start(self):
         if new_node := await self.graph.get_entry_node():
