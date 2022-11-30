40eb57350d7567753b901fdeba27ef8f4bcefe92
Vinzenz Aubry <vinzenzaubry@gmail.com>
2022-11-30 13:22:00 +0100

    interim editor js

---
+++ caster-editor/src/pages/editortest.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>This is the editor test</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Editortest",
+
+  data() {
+    return { };
+  },
+
+  computed: {
+  },
+
+  mounted() {
+    console.log("yeah");
+  }
+};
+</script>
