03d162a0d9038a43fc43883abfd4ac0e8f5c0c98
Vinzenz Aubry <vinzenzaubry@gmail.com>
2022-11-30 19:49:22 +0100

    Update debugBlockData.ts

--- caster-editor/src/assets/js/dev/debugBlockData.ts
+++ caster-editor/src/assets/js/dev/debugBlockData.ts
@@ -6,7 +6,7 @@ const debugBlockData = [
   { cellCode: "# Use of String Formatting\r\nfloat1 = 563.78453\r\nprint(\"{:5.2f}\".format(float1))\r\n\r\n# Use of String Interpolation\r\nfloat2 = 563.78453\r\nprint(\"%5.2f\" % float2)", cellOrder: 1, cellType: "python", uuid: "ea95186e-a3b6-4008-a84d-97d59d0fe804", __typename: "ScriptCell" },
 
   // supercollider
-  { cellCode: "\r\n({RHPF.ar(OnePole.ar(BrownNoise.ar, 0.99), LPF.ar(BrownNoise.ar, 14)\r\n* 400 + 500, 0.03, 0.003)}!2)\r\n+ ({RHPF.ar(OnePole.ar(BrownNoise.ar, 0.99), LPF.ar(BrownNoise.ar, 20)\r\n* 800 + 1000, 0.03, 0.005)}!2)\r\n* 4\r\n}.play", cellOrder: 2, cellType: "supercollider", uuid: "18ea7291-e11a-4f5a-b458-f0bd122edba5", __typename: "ScriptCell" },
+  { cellCode: "({RHPF.ar(OnePole.ar(BrownNoise.ar, 0.99), LPF.ar(BrownNoise.ar, 14)\r\n* 400 + 500, 0.03, 0.003)}!2)\r\n+ ({RHPF.ar(OnePole.ar(BrownNoise.ar, 0.99), LPF.ar(BrownNoise.ar, 20)\r\n* 800 + 1000, 0.03, 0.005)}!2)\r\n* 4\r\n}.play", cellOrder: 2, cellType: "supercollider", uuid: "18ea7291-e11a-4f5a-b458-f0bd122edba5", __typename: "ScriptCell" },
 
   // comment
   { cellCode: "<h2> Hello Comment</h2> <p> this is a <em>basic</em> example of a <strong>comment</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists or anything else you can do via markdown</p>", cellOrder: 3, cellType: "comment", uuid: "1a4a9a65-f5c4-45d2-8ab1-ae392009ab1a", __typename: "ScriptCell" }
