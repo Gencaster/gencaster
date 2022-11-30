ba91bc74ac4e88bbab270bc94a569265fb1b9b7b
Vinzenz Aubry <vinzenzaubry@gmail.com>
2022-11-30 15:26:51 +0100

    added title and menu buttons to node editor

--- caster-editor/src/components/elements/NodeEditor.vue
+++ caster-editor/src/components/elements/NodeEditor.vue
@@ -1,8 +1,28 @@
+<script setup lang="ts">
+import { Plus, Scissor, VideoPause, VideoPlay } from "@element-plus/icons-vue";
+</script>
+
 <template>
   <div class="node-editor-outer">
-    I'm the node editor
-    {{ dev }}
-    {{ blockData }}
+    <div class="title">
+      <div class="left">
+        <p>{{ currentNodeName }}</p>
+        <button class="unstyled" @click="openNodeNameEdit()">
+          edit
+        </button>
+      </div>
+      <div class="right">
+        <button class="unstyled" @click="closeNodeData()">
+          Close
+        </button>
+      </div>
+    </div>
+    <div class="node-menu-bar">
+      <el-button text bg :icon="Plus" />
+      <el-button text bg :icon="Scissor" />
+      <el-button text bg :icon="VideoPlay" />
+      <el-button text bg :icon="VideoPause" />
+    </div>
   </div>
 </template>
 
@@ -20,6 +40,11 @@ export default {
       type: Array,
       required: true,
       default: () => []
+    },
+    currentNodeName: {
+      type: String,
+      required: false,
+      default: () => "Placeholder"
     }
   },
 
@@ -31,6 +56,11 @@ export default {
   },
 
   mounted() {
+  },
+
+  methods: {
+    openNodeNameEdit() {},
+    closeNodeData() {}
   }
 };
 </script>
@@ -39,8 +69,56 @@ export default {
 .node-editor-outer {
   height: inherit;
   widows: inherit;
-  padding: 10px;
+  padding: 5px 15px 15px 15px;
   background-color: $mainWhite;
   border: 1px solid $black;
+
+  .title {
+      display: flex;
+      height: $menuHeight;
+      justify-content: space-between;
+      align-items: center;
+      margin-bottom: 4px;
+
+      .left {
+        button {
+          color: $grey-dark;
+
+          &:hover {
+            font-style: italic;
+            background-color: transparent;
+          }
+        }
+      }
+
+      .right {
+        transform: translateX(8px);
+        text-decoration: underline;
+      }
+
+      .left,
+      .right {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        p {
+          margin: 0;
+        }
+      }
+    }
+
+    .node-menu-bar {
+      display: flex;
+      height: $menuHeight;
+      background-color: $grey-light;
+      border-radius: 4px;
+      margin-bottom: 12px;
+
+      .el-icon svg {
+        transform: scale(1.5);
+      }
+    }
+
 }
 </style>
