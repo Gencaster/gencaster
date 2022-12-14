83df6bc60e438396da09f3f4bfecd9e970ae28f1
Vinzenz Aubry <vinzenzaubry@gmail.com>
2022-12-14 18:43:36 +0100

    Fixed a css bug I created

--- caster-editor/src/pages/dev/blockeditor.vue
+++ caster-editor/src/pages/dev/blockeditor.vue
@@ -13,3 +13,23 @@
 <script setup lang="ts">
 import { debugBlockData } from "@/assets/js/dev/debugBlockData";
 </script>
+
+<style lang="scss">
+.edit-page {
+  .editor-wrapper {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    background-color: white;
+    width: 100vw;
+    height: 100vh;
+    padding: 20px;
+
+    .editor-inner {
+      width: 50%;
+      width: 800px;
+      height: 100%;
+    }
+  }
+}
+</style>
