8e18298cd34b600a39cf686bab1d98fc995a292d
Vinzenz Aubry <vinzenzaubry@gmail.com>
2023-01-10 23:57:49 +0100

    Added Editor rendering for markdown and comment

--- caster-editor/src/components/elements/BlockMarkdown.vue
+++ caster-editor/src/components/elements/BlockMarkdown.vue
@@ -1,18 +1,13 @@
 <template>
   <div class="block">
+    {{}}
     <!-- markdown -->
-    <div
-      v-if="scriptCell?.cellType === CellType.Markdown"
-      class="editor-markdown"
-    >
+    <div v-if="scriptCell?.cellType === CellType.Markdown" class="editor-markdown">
       <div ref="editorDom" />
     </div>
 
     <!-- comment -->
-    <div
-      v-if="scriptCell?.cellType === CellType.Comment"
-      class="editor-comment"
-    >
+    <div v-if="scriptCell?.cellType === CellType.Comment" class="editor-comment">
       <div ref="editorDom" />
     </div>
   </div>
@@ -23,6 +18,7 @@ import EditorJS from "@editorjs/editorjs";
 import Header from "@editorjs/header"; // TODO: Fix Could not find a declaration file for module '@editorjs/header'.
 import type { Ref } from "vue";
 import { storeToRefs } from "pinia";
+import { v4 as uuidv4 } from "uuid";
 import { CellType } from "@/graphql/graphql";
 import type { GetNodeQuery, ScriptCell } from "@/graphql/graphql";
 import { useNodeStore } from "@/stores/NodeStore";
@@ -34,6 +30,22 @@ interface BlockProps {
   index: number
 }
 
+interface Data {
+  text: string
+}
+
+interface Block {
+  id: string
+  type: string
+  data: Data
+}
+
+interface editorJsInterface {
+  time: number
+  blocks: Block[]
+  version: string
+}
+
 // Store
 const { scriptCellsModified, node } = storeToRefs(useNodeStore());
 
@@ -41,38 +53,55 @@ const { scriptCellsModified, node } = storeToRefs(useNodeStore());
 const scriptCell = ref<GetNodeQuery["node"]["scriptCells"][0] | undefined>(node.value.scriptCells.find((x) => { return x.uuid === props.scriptCellUuid; }));
 const editorJS = ref<EditorJS>();
 const editorDom = ref<HTMLElement>();
-const initData = ref();
 
 const editorChange = (api?: any, event?: any) => {
   scriptCellsModified.value = true;
   editorJS.value?.save().then((outputData) => {
+    console.log(outputData);
+
     if (scriptCell.value !== undefined)
       scriptCell.value.cellCode = outputData.blocks.map((x) => { return x.data.text as string; }).join("\n");
   });
 };
 
 onMounted(() => {
+  const splitScriptCell = scriptCell.value?.cellCode.split(/\r\n|\r|\n/) || [];
+
+  const editorJSInitvalue: editorJsInterface = {
+    time: Date.now(),
+    blocks: [],
+    version: "2.26.4"
+  };
+
+  splitScriptCell.forEach((string) => {
+    editorJSInitvalue.blocks.push({
+      id: uuidv4(),
+      type: "paragraph",
+      data: {
+        text: string
+      }
+    });
+  });
+
   editorJS.value = new EditorJS({
     holder: editorDom.value,
     minHeight: 0,
+    data: editorJSInitvalue,
     tools: {
       header: Header
     },
-    // @todo scriptCell.value?.cellCode ??
     onChange: editorChange
   });
 
-  // @todo editorjs does need json but we don't have json in backend
-  // initData.value = JSON.parse(scriptCell.value?.cellCode || "{}");
+  // custom setup for types
+  // switch (scriptCell.value?.cellCode) {
+  //   case CellType.Comment:
+  //     break;
 
-  switch (scriptCell.value?.cellCode) {
-    case CellType.Comment:
-      break;
-
-    case CellType.Markdown:
-      break;
-    default:
-      break;
-  }
+  //   case CellType.Markdown:
+  //     break;
+  //   default:
+  //     break;
+  // }
 });
 </script>
