0e115db416693610f191b2654618309cc5c7e7ff
Dennis Scheiba <dennis.scheiba@gmx.de>
2023-01-13 15:14:37 +0100

    setup urql for subscription use

--- caster-editor/src/plugins/urql.ts
+++ caster-editor/src/plugins/urql.ts
@@ -1,13 +1,22 @@
-import urql from "@urql/vue";
+import urql, { defaultExchanges, subscriptionExchange } from "@urql/vue";
+import { SubscriptionClient } from "subscriptions-transport-ws";
 
 import { defineNuxtPlugin } from "#app";
 import { useRuntimeConfig } from "#imports";
+
+const subscriptionClient = new SubscriptionClient("ws://127.0.0.1:8081/graphql", { reconnect: true });
 
 export default defineNuxtPlugin((nuxtApp: any) => {
   const config = useRuntimeConfig();
 
   nuxtApp.vueApp.use(urql, {
     url: config.public.BACKEND_GRAPHQL_URL,
+    exchanges: [
+      ...defaultExchanges,
+      subscriptionExchange({
+        forwardSubscription: operation => subscriptionClient.request(operation)
+      })
+    ],
     requestPolicy: "network-only",
     fetchOptions: {
       credentials: "include"
