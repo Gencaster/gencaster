31806f5421333d11c1d6ed5497d6e1e860137bb2
Vinzenz Aubry <vinzenzaubry@gmail.com>
2023-01-20 00:00:08 +0100

    removed editor js

--- caster-editor/src/components/elements/BlockMarkdown.vue
+++ caster-editor/src/components/elements/BlockMarkdown.vue
@@ -2,24 +2,26 @@
   <div class="block">
     <!-- markdown -->
     <div v-if="scriptCell?.cellType === CellType.Markdown" class="editor-markdown">
-      <div ref="editorDom" />
+      <p>This is markdown</p>
+      <!-- <div ref="editorDom" /> -->
     </div>
 
     <!-- comment -->
     <div v-if="scriptCell?.cellType === CellType.Comment" class="editor-comment">
-      <div ref="editorDom" />
+      <p>This is a comment</p>
+      <!-- <div ref="editorDom" /> -->
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import EditorJS from "@editorjs/editorjs";
-import Header from "@editorjs/header"; // TODO: Fix Could not find a declaration file for module '@editorjs/header'.
-import type { Ref } from "vue";
+// import EditorJS from "@editorjs/editorjs";
+// import Header from "@editorjs/header"; // TODO: Fix Could not find a declaration file for module '@editorjs/header'.
+
 import { storeToRefs } from "pinia";
-import type { OutputData } from "@editorjs/editorjs";
+// import type { OutputData } from "@editorjs/editorjs";
 import { CellType } from "@/graphql/graphql";
-import type { GetNodeQuery, ScriptCell } from "@/graphql/graphql";
+import type { GetNodeQuery } from "@/graphql/graphql";
 import { useNodeStore } from "@/stores/NodeStore";
 
 const props = defineProps<BlockProps>();
@@ -34,47 +36,47 @@ const { scriptCellsModified, node } = storeToRefs(useNodeStore());
 
 // Variables
 const scriptCell = ref<GetNodeQuery["node"]["scriptCells"][0] | undefined>(node.value.scriptCells.find((x) => { return x.uuid === props.scriptCellUuid; }));
-const editorJS = ref<EditorJS>();
+// const editorJS = ref<EditorJS>();
 const editorDom = ref<HTMLElement>();
 
-const editorChange = (api?: any, event?: any) => {
-  scriptCellsModified.value = true;
-  editorJS.value?.save().then((outputData) => {
-    console.log(outputData);
+// const editorChange = (api?: any, event?: any) => {
+//   scriptCellsModified.value = true;
+//   editorJS.value?.save().then((outputData) => {
+//     console.log(outputData);
 
-    if (scriptCell.value !== undefined)
-      scriptCell.value.cellCode = outputData.blocks.map((x) => { return x.data.text as string; }).join("\n");
-  });
-};
+//     if (scriptCell.value !== undefined)
+//       scriptCell.value.cellCode = outputData.blocks.map((x) => { return x.data.text as string; }).join("\n");
+//   });
+// };
 
 onMounted(() => {
   const splitScriptCell = scriptCell.value?.cellCode.split(/\r\n|\r|\n/) || [];
 
-  const editorJSInitvalue: OutputData = {
-    time: Date.now(),
-    blocks: [],
-    version: "2.26.4"
-  };
+  // const editorJSInitvalue: OutputData = {
+  //   time: Date.now(),
+  //   blocks: [],
+  //   version: "2.26.4"
+  // };
 
-  splitScriptCell.forEach((string) => {
-    editorJSInitvalue.blocks.push({
-      id: self.crypto.randomUUID(),
-      type: "paragraph",
-      data: {
-        text: string
-      }
-    });
-  });
+  // splitScriptCell.forEach((string) => {
+  //   editorJSInitvalue.blocks.push({
+  //     id: self.crypto.randomUUID(),
+  //     type: "paragraph",
+  //     data: {
+  //       text: string
+  //     }
+  //   });
+  // });
 
-  editorJS.value = new EditorJS({
-    holder: editorDom.value,
-    minHeight: 0,
-    data: editorJSInitvalue,
-    tools: {
-      header: Header
-    },
-    onChange: editorChange
-  });
+  // editorJS.value = new EditorJS({
+  //   holder: editorDom.value,
+  //   minHeight: 0,
+  //   data: editorJSInitvalue,
+  //   tools: {
+  //     header: Header
+  //   },
+  //   onChange: editorChange
+  // });
 
   // custom setup for types
   // switch (scriptCell.value?.cellCode) {
