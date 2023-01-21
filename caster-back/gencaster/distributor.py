6311e023c61165fdf4485f2ebd56e500d4629083
Dennis Scheiba <dennis.scheiba@gmx.de>
2023-01-21 17:25:07 +0100

    add channel for node updates

--- caster-back/gencaster/distributor.py
+++ caster-back/gencaster/distributor.py
@@ -1,7 +1,7 @@
 import logging
 import uuid
 from dataclasses import asdict, dataclass, field
-from typing import AsyncGenerator, List
+from typing import AsyncGenerator, List, Union
 
 from channels.layers import InMemoryChannelLayer
 from strawberry.channels import GraphQLWSConsumer
@@ -28,6 +28,7 @@ class GenCasterChannel:
     """
 
     GRAPH_UPDATE_TYPE = "graph.update"
+    NODE_UPDATE_TYPE = "node.update"
 
     def __init__(self) -> None:
         pass
@@ -40,7 +41,16 @@ class GenCasterChannel:
         )
 
     @staticmethod
-    async def send_message(layer: InMemoryChannelLayer, message: "GraphUpdateMessage"):
+    async def send_node_update(layer: InMemoryChannelLayer, node_uuid: uuid.UUID):
+        return await GenCasterChannel.send_message(
+            layer=layer, message=NodeUpdateMessage(uuid=node_uuid)
+        )
+
+    @staticmethod
+    async def send_message(
+        layer: InMemoryChannelLayer,
+        message: Union["GraphUpdateMessage", "NodeUpdateMessage"],
+    ):
         for channel in message.channels:
             await layer.group_send(channel, asdict(message))
 
@@ -49,7 +59,6 @@ class GenCasterChannel:
         consumer: GraphQLWSConsumer, graph_uuid: uuid.UUID
     ) -> AsyncGenerator["GraphUpdateMessage", None]:
         group_name = uuid_to_group(graph_uuid)
-        consumer.channel_name
         if not consumer.channel_layer:
             raise MissingChannelLayer()
         await consumer.channel_layer.group_add(group_name, consumer.channel_name)
@@ -58,11 +67,37 @@ class GenCasterChannel:
         ):
             yield GraphUpdateMessage(**message)
 
+    @staticmethod
+    async def receive_node_updates(
+        consumer: GraphQLWSConsumer,
+        node_uuid: uuid.UUID,
+    ) -> AsyncGenerator["NodeUpdateMessage", None]:
+        group_name = uuid_to_group(node_uuid)
+        if not consumer.channel_layer:
+            raise MissingChannelLayer()
+        await consumer.channel_layer.group_add(group_name, consumer.channel_name)
+        async for message in consumer.channel_listen(
+            GenCasterChannel.NODE_UPDATE_TYPE,
+            groups=[group_name],
+        ):
+            yield NodeUpdateMessage(**message)
+
 
 @dataclass
 class GraphUpdateMessage:
     uuid: uuid.UUID
     type: str = GenCasterChannel.GRAPH_UPDATE_TYPE
+    additional_channels: List[str] = field(default_factory=list)
+
+    @property
+    def channels(self) -> List[str]:
+        return [uuid_to_group(self.uuid)] + self.additional_channels
+
+
+@dataclass
+class NodeUpdateMessage:
+    uuid: uuid.UUID
+    type: str = GenCasterChannel.NODE_UPDATE_TYPE
     additional_channels: List[str] = field(default_factory=list)
 
     @property
