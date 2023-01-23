a6da0c11cb30858d55deec99be053292a9a337c0
Dennis Scheiba <dennis.scheiba@gmx.de>
2023-01-23 17:33:21 +0100

    configure subscription client

--- caster-front/src/main.ts
+++ caster-front/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import urql from "@urql/vue";
+import urql, { defaultExchanges, subscriptionExchange } from "@urql/vue";
+import { SubscriptionClient } from "subscriptions-transport-ws";
 
 import App from "./App.vue";
 import router from "./router";
@@ -14,7 +15,13 @@ app.use(urql, {
   requestPolicy: "network-only",
   fetchOptions: {
     credentials: "include"
-  }
+  },
+  exchanges: [
+    ...defaultExchanges,
+    subscriptionExchange({
+      forwardSubscription: operation => new SubscriptionClient((import.meta.env.VITE_BACKEND_GRAPHQL_URL || "http://127.0.0.1:8081/graphql").replaceAll("https", "wss").replaceAll("http", "ws"), { reconnect: true }).request(operation)
+    })
+  ]
 });
 app.use(createPinia());
 app.use(router);
