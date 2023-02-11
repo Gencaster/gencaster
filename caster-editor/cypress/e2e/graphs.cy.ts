41c75bce740846db8681c86932743aef60afed19
Dennis Scheiba <dennis.scheiba@gmx.de>
2023-02-11 18:05:48 +0100

    add graph creation frontend tests

--- caster-editor/cypress/e2e/graphs.cy.ts
+++ caster-editor/cypress/e2e/graphs.cy.ts
@@ -1,27 +1,13 @@
-import { aliasQuery } from '../utils';
+import { aliasMutation, aliasQuery, hasOperationName } from '../utils';
 
 describe('Graphs overview page', () => {
     beforeEach(() => {
         cy.intercept('POST', 'http://127.0.0.1:8081/graphql', (req) => {
             // Queries
-            aliasQuery(req, 'GetGraphs')
+            aliasQuery(req, 'GetGraphs');
+            aliasMutation(req, 'CreateGraph');
         });
     });
-
-    // it('login on error', () => {
-    //     cy.intercept('POST', 'http://127.0.0.1:8081/graphql', (req) => {
-    //         // from https://docs.cypress.io/guides/end-to-end-testing/working-with-graphql
-    //         if (hasOperationName(req, 'GetGraphs')) {
-    //             req.alias = 'gqlGetGraphsQuery'
-    //             req.reply((res) => {
-    //                 res.body.data = undefined
-    //                 res.body.errors = [{ error: "error", name: "no auth" }];
-    //             })
-    //         }
-    //     })
-    //     cy.visit('/graphs');
-    //     cy.contains('Please login');
-    // });
 
     it('show uuid of graphs', () => {
         cy.intercept('POST', 'http://127.0.0.1:8081/graphql', { fixture: 'graphs.json' });
@@ -30,12 +16,45 @@ describe('Graphs overview page', () => {
         cy.contains("b611fdaf-331f-4853-b10f-f593e181f115");
     });
 
-    it('create new graph', () => {
-        cy.intercept('POST', 'http://127.0.0.1:8081/graphql', { fixture: 'graphs.json' });
+    it('mock create new graph', () => {
+        cy.intercept('POST', 'http://127.0.0.1:8081/graphql', (req) => {
+            if (hasOperationName(req, 'GetGraphs')) {
+                req.reply({fixture: "graphs.json"});
+            }
+            if (hasOperationName(req, 'CreateGraph')) {
+                req.alias = 'gqlCreateGraphMutation';
+                req.reply({fixture: "createGraph.json"});
+            }
+
+        });
         cy.visit('/graphs');
         cy.get(':last-child > .graph > div').click();
+        cy.get('#graphNameInput').type("my_test_graph");
+        cy.get('.el-button--primary > span').click();
+        cy.wait('@gqlCreateGraphMutation');
+        cy.wait(1000);
+        cy.get('#graphNameInput').should('not.visible', {timeout: 10000});
+    });
+
+    it('error when creating same named graph', () => {
+        cy.intercept('POST', 'http://127.0.0.1:8081/graphql', (req) => {
+            if (hasOperationName(req, 'GetGraphs')) {
+                req.reply({fixture: "graphs.json"});
+            }
+            if (hasOperationName(req, 'CreateGraph')) {
+                req.alias = 'gqlCreateGraphMutation';
+                req.reply({fixture: "createGraphSameError.json"});
+            }
+        });
+        cy.visit('/graphs');
+        cy.get(':last-child > .graph > div').click();
+        cy.get('#graphNameInput').type("my_test_graph");
+        cy.get('.el-button--primary > span').click();
+        cy.wait('@gqlCreateGraphMutation');
+        cy.wait(1000);
+        cy.get('#graphNameInput').should('be.visible', {timeout: 10000});
         cy.on('window:alert', (t) => {
-            expect(t).to.contains('tbd');
+            expect(t).to.contains('Could not create graph');
         })
     });
 })
