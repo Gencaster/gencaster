d694fc6c7c9060203c035aef06e172a502565ef2
Dennis Scheiba <dennis.scheiba@gmx.de>
2023-02-21 22:57:20 +0100

    clean root template

--- caster-front/src/App.vue
+++ caster-front/src/App.vue
@@ -1,20 +1,7 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">
-          Home
-        </RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
-
-<style scoped>
-</style>
