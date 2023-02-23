d5b67e61b9022ce4a6e277c5be155dc68e514ec3
Dennis Scheiba <dennis.scheiba@gmx.de>
2023-02-23 14:56:30 +0100

    create additional views

--- caster-front/src/router/index.ts
+++ caster-front/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "@/views/HomeView.vue";
+import AdminView from "@/views/AdminView.vue";
+import GraphPlayerView from "@/views/GraphPlayerView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,15 +10,18 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView
+    },
+    {
+      path: "/admin",
+      name: "admin",
+      component: AdminView
+    },
+    {
+      path: "/graph/:graphUuid/player",
+      name: "graphPlayer",
+      component: GraphPlayerView
     }
-    // {
-    //   path: "/about",
-    //   name: "about",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import("../views/AboutView.vue")
-    // }
+
   ]
 });
 
