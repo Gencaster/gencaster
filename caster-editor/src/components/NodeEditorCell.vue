60e3e51e749918b8aaddc1cc04a915633f7382d2
Vinzenz Aubry <vinzenzaubry@gmail.com>
2023-04-14 15:24:10 +0200

    Update NodeEditorCell.vue

--- caster-editor/src/components/NodeEditorCell.vue
+++ caster-editor/src/components/NodeEditorCell.vue
@@ -88,17 +88,17 @@ const scriptCellText = computed<string>({
   }
 });
 
-const sriptCellAudioCell = computed<ScriptCellData['audioCell']>({
-  get() {
-    return props.scriptCell.audioCell ?? undefined;
-  },
-  set(value) {
-    const newCell = {...props.scriptCell};
-    newCell.audioCell = value;
-    emit('update:scriptCell', newCell);
-    return value;
-  }
-});
+// const sriptCellAudioCell = computed<ScriptCellData['audioCell']>({
+//   get() {
+//     return props.scriptCell.audioCell ?? undefined;
+//   },
+//   set(value) {
+//     const newCell = {...props.scriptCell};
+//     newCell.audioCell = value;
+//     emit('update:scriptCell', newCell);
+//     return value;
+//   }
+// });
 
 const displayError = async(message: string) => {
   ElMessage({
