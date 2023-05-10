c0736150181d524da84f14e6a7c15465f4e393d4
Dennis Scheiba <dennis.scheiba@gmx.de>
2023-05-10 14:36:46 +0200

    introduce template switching for graph detail view

---
+++ caster-front/src/components/GraphDetailTemplates/Default.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import GraphPlayer from "@/components/GraphPlayer.vue";
+import type { Graph } from "@/graphql";
+
+defineProps<{
+  graph: Pick<Graph, "uuid" | "name">
+}>();
+</script>
+
+<template>
+  <div class="default-graph-detail">
+    <GraphPlayer
+      :graph="graph"
+    />
+  </div>
+</template>
---
+++ caster-front/src/components/GraphDetailTemplates/Drifter.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import GraphPlayer from "@/components/GraphPlayer.vue";
+import type { Graph } from "@/graphql";
+
+defineProps<{
+  graph: Pick<Graph, "uuid" | "name">
+}>();
+</script>
+
+<template>
+  <div class="default-graph-detail">
+    <h1>Drifter</h1>
+    <GraphPlayer
+      :graph="graph"
+    />
+  </div>
+</template>
