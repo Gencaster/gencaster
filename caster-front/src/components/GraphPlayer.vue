813ecb4994c94dadc6f34b58123ca20951b9625e
Vinzenz Aubry <vinzenzaubry@gmail.com>
2023-05-15 00:46:12 +0200

    set debug back to true

--- caster-front/src/components/GraphPlayer.vue
+++ caster-front/src/components/GraphPlayer.vue
@@ -30,7 +30,7 @@ const {
 
 const router = useRouter();
 // const showDebug = computed<boolean>(() => router.currentRoute.value.query.debug === null);
-const showDebug = false;
+const showDebug = true;
 
 const { data, error, stale } = useStreamSubscription({
   variables: {
